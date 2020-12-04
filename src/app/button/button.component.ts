76c75e45bd86723445d6fb3b7b5bfda00c58f0f6
Jakub Pánek <43809342+Soxori@users.noreply.github.com>
2020-12-04 17:42:35 +0100

    .

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
 
-  constructor() { }
+  public score = 0;
+
+  //statsService
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  clickedButton(): void {
+
   }
 
 }
